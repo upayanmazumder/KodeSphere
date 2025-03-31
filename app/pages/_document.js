6301d411e89e3d3a7f971e7c10fb75c96b1dcb3b
Upayan Mazumder <upayanm3@gmail.com>
2025-03-31 15:05:25 +0530

    Add webp icons and update document head for improved performance and SEO

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,9 +1,14 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>kodesphere</title>
+        <meta name="description" content="Deploy Smarter, Not Harder" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
